eebf982da60666c7c0cf60aaa27deb610320f043
sirwisdom <48869868+sirwisdom@users.noreply.github.com>
2019-08-14 23:31:43 -1200

    first push
    first push at internship with Afridash

---
+++ build/precache-manifest.c3040301d02cdf885f69f33a434ff7fc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c956225641dcff9cadaba8539c102b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "942452bbaee1f9731071",
+    "url": "/static/css/main.45081983.chunk.css"
+  },
+  {
+    "revision": "27f970365489ab317c7c",
+    "url": "/static/js/2.1a543e43.chunk.js"
+  },
+  {
+    "revision": "942452bbaee1f9731071",
+    "url": "/static/js/main.491f3a80.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
